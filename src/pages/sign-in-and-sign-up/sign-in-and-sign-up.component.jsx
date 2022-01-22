25702e815f36adf394cf86bd098766b0c1df7ee3
Olivier Dorvelus <ollydorvelus@gmail.com>
2022-01-22 16:37:51 -0500

    sign in form

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => {
+    return (<SignIn />)
+}
+
+export default SignInAndSignUpPage;
